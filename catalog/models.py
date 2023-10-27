2ff1834f547f13923355c653a0b014640689c7bb
Python <python@it.step>
2023-10-27 21:19:32 +0300

    klaswork

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField('Category', max_length=50, null=False, blank=False)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f'{self.pk} - {self.name}'
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
