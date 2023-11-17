e76d019c62e0a3d03720df392db9393a91f7e504
Python <python@it.step>
2023-11-17 21:03:12 +0300

    newcomit

--- profiles/views.py
+++ profiles/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 import logging
 from django.contrib import messages
+from .decorators import unauthenticated_user
+from .forms import CreateUserForm
 
 
 logger = logging.getLogger('movie_logger')
 
 
+@unauthenticated_user
 def login_user(request):
     # breakpoint()
     if request.method == 'POST':
@@ -20,3 +23,24 @@ def login_user(request):
             messages.info(request, 'Username or password do not match')
             logger.info('Username or password do not match')
     return render(request, 'login.html')
+
+
+@unauthenticated_user
+def register_user(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.info(request, f'The user {username} has been registered')
+            return redirect('login')
+    context = {
+        'form': form
+    }
+    return render(request, 'register1.html', context=context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
